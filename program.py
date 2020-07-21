6b25d755100f8c548c0fec13c94701915379aa58
ilyas <ilyasyusuf018@gmail.com>
2020-07-21 17:09:21 +0700

    ea

--- program.py
+++ program.py
@@ -226,39 +226,115 @@ def refresh(var):
 def my_python_method(param1):
     print(param1)
 @eel.expose
-def main(param1):
+def bukacam():
+	global known_face_encodings, known_face_names, video_capture, emails, currenttime
+	update()
+	# Initialize some variables
+	face_locations = []
+	face_encodings = []
+	face_names = []
+	process_this_frame = True
+	i=0
+	unknown_face = []
+
+	
     # creating object of TwitterClient Class 
-	api = TwitterClient() 
-	# calling function to get tweets 
-	tweets = api.get_tweets(query = param1, count = 400)
-	# picking positive tweets from tweets 
-	ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
-	# percentage of positive tweets 
-	print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets))) 
-	# picking negative tweets from tweets 
-	ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative'] 
-	# percentage of negative tweets 
-	print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets))) 
-	ptper = round(100*len(ptweets)/len(tweets),0)
-	ntper = round(100*len(ntweets)/len(tweets),0)
-	nnper = round((100 *(len(tweets) - len(ntweets) - len(ptweets)) / len(tweets)),0)
-	panjang = len(tweets)
-	persen = [ptper,ntper,nnper,panjang]
-	nettweets = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral'] 
-	# percentage of neutral tweets 
-	print("Neutral tweets percentage: {} % ".format(100*(len(tweets) - len(ntweets) - len(ptweets)) /len(tweets)))    
-	# printing first 5 positive tweets 
-	print("\n\nPositive tweets:") 
-	# for tweet in ptweets[:10]: 
+	while True:
+			# Grab a single frame of video
+			ret, frame = video_capture.read()
+			# frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-	#     print(tweet['text']) 
-	# # printing first 5 negative tweets 
-	# print("\n\nNegative tweets:") 
-	# for tweet in ntweets[:10]: 
-	#     print(tweet['text'])
+			# Resize frame of video to 1/4 size for faster face recognition processing
+			small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-	return ptweets,ntweets,persen,nettweets; 
+			# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+			rgb_small_frame = small_frame[:, :, ::-1]
 
+			# Only process every other frame of video to save time
+			if process_this_frame:
+				# Find all the faces and face encodings in the current frame of video
+				face_locations = face_recognition.face_locations(rgb_small_frame)
+				face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+				face_names = []
+				for face_encoding in face_encodings:
+					# See if the face is a match for the known face(s)
+					matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+					name = "unknown"
+
+					# # If a match was found in known_face_encodings, just use the first one.
+					# if True in matches:
+					#     first_match_index = matches.index(True)
+					#     name = known_face_names[first_match_index]
+
+					# Or instead, use the known face with the smallest distance to the new face
+					face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+					best_match_index = np.argmin(face_distances)
+					if matches[best_match_index]:
+						name = known_face_names[best_match_index]
+						email = emails[best_match_index]
+					# else:
+					# 	print(i)
+
+					face_names.append(name)
+
+			process_this_frame = not process_this_frame
+
+
+			# Display the results
+			for (top, right, bottom, left), name in zip(face_locations, face_names):
+				# Scale back up face locations since the frame we detected in was scaled to 1/4 size
+				top *= 4
+				right *= 4
+				bottom *=4 
+				left *= 4
+
+				# Draw a box around the face
+				cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 1)
+				if(time.time() - currenttime > 59):
+					update()
+					print('data updated')
+					currenttime = time.time()
+				# Draw a label with a name below the face
+				cv2.rectangle(frame, (left, bottom - 20), (right, bottom), (0, 255, 0), cv2.FILLED)
+				font = cv2.FONT_HERSHEY_DUPLEX
+				cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5 , (255, 255, 255), 1)
+				if(name != 'unknown'):
+					mydb.commit()                                   
+					query = "SELECT nim FROM absensi where nim = '{}' and date_format(created_at,'%Y-%m-%{}') = CURDATE()".format(name,str('d'))
+					mycursor.execute(query)
+					hasil = mycursor.fetchall()
+					# print(len(hasil))
+					if(len(hasil)<1):
+						newQuery = "insert into absensi (nim,status) values(%s,%s)"
+						values = (name,"hadir")
+						mycursor.execute(newQuery,values)
+						mydb.commit()
+						fromaddr = 'hidroponikapps@gmail.com'
+						toaddrs  = email
+						hari = datetime.today().strftime('%Y-%m-%d').format(1)
+						msg = 'Hi, {} <br>Anda sudah absen pada tanggal {}'.format(name,hari)
+						username = 'hidroponikapps@gmail.com'
+						password = 'aizrxwyqyotgxnyr'
+						server = smtplib.SMTP('smtp.gmail.com:587')
+						server.starttls()
+						server.login(username,password)
+						server.sendmail(fromaddr, toaddrs, msg)
+						server.quit()
+				else:
+					print(name)
+					
+
+			# Display the resulting image
+			cv2.imshow('Video', frame)
+
+			# Hit 'q' on the keyboard to quit!
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+
+		# Release handle to the webcam
+	video_capture.release()
+	cv2.destroyAllWindows()
 
 @eel.expose
 def getTime():
